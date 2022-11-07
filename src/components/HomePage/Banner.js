7bf9684af2fd0dca5eb7dff361d2947f8327ab81
unknown <julianatonu@gmail.com>
2022-11-07 22:33:02 +0600

    added banner

---
+++ src/components/HomePage/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='rounded'>
+            <div className="hero min-h-screen " style={{ backgroundImage: `url("https://cdn0.weddingwire.in/article/9086/3_2/960/jpg/16809-creative-wedding-photography-avinash-dhoundhiyal-photography-lead-image.jpeg")` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5  text-6xl font-bold "> Turn Your Event into Memories</h1>
+      <p className="mb-5">Your each movement, your each emotion is nothing but a beautiful picture and I just want to hold on those moments with camera and make it a piece of art with adroit editing.</p>
+     
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+
+export default Banner;
