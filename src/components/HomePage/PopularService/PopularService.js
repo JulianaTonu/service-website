ebefd2c05c611e79e3028efe160a694f2b28e615
unknown <julianatonu@gmail.com>
2022-11-08 21:50:44 +0600

    added react-image-viewer-hook for viewed full picture

--- src/components/HomePage/PopularService/PopularService.js
+++ src/components/HomePage/PopularService/PopularService.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useImageViewer } from "react-image-viewer-hook";
 
 const PopularService = ({service}) => {
     const {title,img,price,details, _id}=service
-       
+    const { getOnClick, ImageViewer } = useImageViewer();
     return (
         <div className="card card-compact w-80   bg-base-100 shadow-xl">
-  <figure className='h-26 '><img src={img} alt="Shoes"/></figure>
+   <figure className='h-26 '><img src={`${img}?auto=compress&cs=tinysrgb&w=300`} onClick={getOnClick(`${img}?auto=compress&cs=tinysrgb&w=1200`)} alt=''/></figure>
+
   <div className="card-body">
     <h2 className="card-title text-2xl">{title}</h2>
     <h2 className="card-title text-2xl font-bold text-primary">{price}</h2>
@@ -17,6 +19,7 @@ const PopularService = ({service}) => {
  <Link to={`/services/${_id}`}> <button className="btn btn-primary">View Details</button></Link>
 </div>
   </div> 
+  < ImageViewer />
 </div>
     );
 };
--- src/components/Services/ServiceCard.js
+++ src/components/Services/ServiceCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { useImageViewer } from "react-image-viewer-hook";
 const ServiceCard = ({service}) => {
     const {title,img,price,details, _id}=service
-       
+    const { getOnClick, ImageViewer } = useImageViewer();
         return (
+
+
             <div className="card card-compact w-80   bg-base-100 shadow-xl">
-      <figure className='h-26 '><img src={img} alt="Shoes"/></figure>
+      <figure className='h-26 '><img src={`${img}?auto=compress&cs=tinysrgb&w=300`} onClick={getOnClick(`${img}?auto=compress&cs=tinysrgb&w=1200`)} alt=''/></figure>
       <div className="card-body">
         <h2 className="card-title text-2xl">{title}</h2>
         <h2 className="card-title text-2xl font-bold text-primary">{price}</h2>
@@ -17,8 +19,22 @@ const ServiceCard = ({service}) => {
      <Link to={`/services/${_id}`}> <button className="btn btn-primary">View Details</button></Link>
     </div>
       </div> 
+      <ImageViewer></ImageViewer>
     </div>
         );
 };
 
 export default ServiceCard;
+
+
+{/* <div className="gallery">
+        {IMAGES.map((src) => (
+          <a
+            key={src}
+            href={`${src}?auto=compress&cs=tinysrgb&w=1200`}
+            onClick={getOnClick(`${src}?auto=compress&cs=tinysrgb&w=1200`)}
+          >
+            <img src={`${src}?auto=compress&cs=tinysrgb&w=300`} />
+          </a>
+        ))}
+      </div> */}
