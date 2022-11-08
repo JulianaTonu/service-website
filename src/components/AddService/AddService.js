d2a5c677cf4ea440f9d29a8a8d6d13a1b34b7c0f
unknown <julianatonu@gmail.com>
2022-11-08 17:42:15 +0600

    add post for create new service

--- src/components/AddService/AddService.js
+++ src/components/AddService/AddService.js
@@ -5,17 +5,31 @@ const AddService = () => {
     const [service, setService]=useState({})
 
     const handleAddService=event=>{
-event.preventDefault()
-console.log(service)
+    event.preventDefault()
+    console.log(service)
+
+    fetch('http://localhost:5000/services',{
+
+     method:'POST',
+     headers:{
+        'content-type':'application/json'
+     },
+     body: JSON.stringify(service)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+    })
     }
 
     const handleInputBlur =event=>{
-const field =event.target.name
-const value =event.target.value
-const newService ={...service}
-newService[field]=value;
-setService(newService)
-    }
+    const field =event.target.name
+    const value =event.target.value
+    const newService ={...service}
+    newService[field]=value;
+    setService(newService)
+    } 
+
     return (
         <Form onSubmit={handleAddService}>
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-10 bg-slate-300'>
@@ -23,8 +37,10 @@ setService(newService)
           
 
             <input onBlur={handleInputBlur} type="text" name='title' placeholder='Title' className="input input-bordered input-primary w-full"  />
-            <input onBlur={handleInputBlur} type="text" name='imgUrl' placeholder='imgUrl' className="input input-bordered input-primary w-full"  />
+            <input onBlur={handleInputBlur} type="text" name='img' placeholder='ImgUrl' className="input input-bordered input-primary w-full"  />
             <input onBlur={handleInputBlur} type="text" name='price' placeholder='Price' className="input input-bordered input-primary w-full"  />
+            <input onBlur={handleInputBlur} type="text" name='time' placeholder='time' className="input input-bordered input-primary w-full"  />
+
             <input onBlur={handleInputBlur} type="text" name='details' placeholder='Details' className="input input-bordered input-primary w-full"  />
 
             <br />
