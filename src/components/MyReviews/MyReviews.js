31210098c5331e40a5d95f0edc266baeab787a60
unknown <julianatonu@gmail.com>
2022-11-09 16:07:05 +0600

    added no review message for myreview page

--- src/components/MyReviews/MyReviews.js
+++ src/components/MyReviews/MyReviews.js
@@ -6,6 +6,7 @@ const MyReviews = () => {
     const {user}=useContext(AuthContext)
     const [reviews, setReviews]=useState([])
 
+    
     useEffect(()=>{
 
         fetch(`http://localhost:5000/reviews?email=${user?.email}`)
@@ -13,7 +14,14 @@ const MyReviews = () => {
         .then(data=>setReviews(data))
     },[user?.email])
    
+
+    if(reviews.length === 0){
+        return<h1 className='text-red-500 font-bold text-3xl text-center my-32  '>No Review Were Added</h1>
+    }
+    console.log('reviiiiiiewwwww',reviews)
     return (
+     <>
+     
         <div className='grid grid-cols-1 lg:grid-cols-1 gap-y-4'>
 {
 reviews.map(review=><MyReviewCard
@@ -22,6 +30,8 @@ review={review}
 ></MyReviewCard>)
 }
 </div>
+     
+     </>
 
     );
 };
